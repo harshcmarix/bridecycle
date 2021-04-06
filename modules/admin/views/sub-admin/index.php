c430b9cf1321f150a377ade99756648ff57e324c
devang kuvardiya <devang.cmarix@gmail.com>
2021-04-06 16:10:10 +0530

    install kartik gridview and implement on sub-admin index page

--- modules/admin/views/sub-admin/index.php
+++ modules/admin/views/sub-admin/index.php
@@ -1,54 +1,93 @@
 <?php
-
+use kartik\grid\GridView;
 use yii\helpers\Html;
-use yii\grid\GridView;
+use yii\helpers\ArrayHelper;
+use kartik\editable\Editable;
 
-/* @var $this yii\web\View */
-/* @var $searchModel app\modules\admin\models\search\SubAdminSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Sub Admin';
-$this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="sub-admin-index">
+$gridColumns = [
+[
+    'attribute' => 'id',
+    'value' => function ($model) {
+        return $model->id;
+    },
+    'header'=>'',
+    'headerOptions'=>['class'=>'kartik-sheet-style']
+],
+[
+ 'attribute' => 'first_name',
+    'value' => function ($model) {
+        return $model->first_name;
+    },
+    'header'=>'',
+    'headerOptions'=>['class'=>'kartik-sheet-style']
+],
+[
+ 'attribute' => 'last_name',
+    'value' => function ($model) {
+        return $model->last_name;
+    },
+    'header'=>'',
+    'headerOptions'=>['class'=>'kartik-sheet-style']
+],
+[
+ 'attribute' => 'email',
+    'value' => function ($model) {
+        return $model->email;
+    },
+    'header'=>'',
+    'headerOptions'=>['class'=>'kartik-sheet-style']
+],
+[
+    'class' => 'kartik\grid\ActionColumn',
+],
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Sub Admin', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'id',
-            'first_name',
-            'last_name',
-            'email:email',
-            //'password_hash',
-            //'temporary_password',
-            //'access_token',
-            //'access_token_expired_at',
-            //'password_reset_token',
-            //'mobile',
-            //'weight',
-            //'height',
-            //'personal_information:ntext',
-            //'user_type',
-            //'is_shop_owner',
-            //'shop_name',
-            //'shop_email:email',
-            //'shop_phone_number',
-            //'created_at',
-            //'updated_at',
-
-            ['class' => 'yii\grid\ActionColumn'],
+];
+echo GridView::widget([
+    'id' => 'kv-grid-demo',
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => $gridColumns, // check the configuration for grid columns by clicking button above
+    'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
+    'headerRowOptions' => ['class' => 'kartik-sheet-style'],
+    'filterRowOptions' => ['class' => 'kartik-sheet-style'],
+    'pjax' => true, // pjax is set to always true for this demo
+    // set your toolbar
+    'toolbar' =>  [
+        [
+            'content' =>
+                Html::button('Add Sub Admin', [
+                    'class' => 'btn btn-success',
+                    'title' => \Yii::t('kvgrid', 'Add Sub Admin'),
+                    'onclick' => "window.location.href = '" . \Yii::$app->urlManager->createUrl(['/admin/sub-admin/create']) . "';",
+                ]), 
+            'options' => ['class' => 'btn-group mr-2']
         ],
-    ]); ?>
+        '{export}',
+        '{toggleData}',
+    ],
+    'toggleDataContainer' => ['class' => 'btn-group mr-2'],
+    // set export properties
+    'export' => [
+        'fontAwesome' => true
+    ],
+    // parameters from the demo form
+    'bordered' => true,
+    'striped' => true,
+    'condensed' => true,
+    'responsive' => true,
+    // 'hover' => $hover,
+    // 'showPageSummary' => $pageSummary,
+    'panel' => [
+        'type' => GridView::TYPE_PRIMARY,
+        'heading' => 'Sub Admin',
+    ],
+    'persistResize' => false,
+    'toggleDataOptions' => ['minCount' => 10],
+    //'exportConfig' => $exportConfig,
+    'itemLabelSingle' => 'Sub Admin',
+    'itemLabelPlural' => 'Sub Admins'
+]);
 
 
-</div>
+?>
