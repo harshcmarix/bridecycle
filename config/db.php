4a4a930eeddbaf3dc5eef1e7fcdcee30d61a022a
ctpl205 <chiragg.cmarix@gmail.com>
2021-09-03 15:25:24 +0530

    Added designer css file and other files.

--- config/db.php
+++ config/db.php
@@ -10,18 +10,32 @@ $config = [
     'queryCacheDuration' => 3600,
 ];
 
-if (YII_ENV == 'dev') {
-    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
-    $config['username'] = 'root';
-    $config['password'] = 'admin';
-} else if (YII_ENV == 'test') {
-    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
-    $config['username'] = 'root';
-    $config['password'] = 'admin';
-} else if (YII_ENV == 'prod') {
-    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
-    $config['username'] = 'root';
-    $config['password'] = 'admin';
-}
+//if (YII_ENV == 'dev') {
+//    /** Stage server */
+//    $config['dsn'] = 'mysql:host=203.109.113.157;dbname=bridecycle';
+//    $config['username'] = 'bridecycle';
+//    $config['password'] = 'bridecycle@#!2021';
+//    /** local */
+////    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
+////    $config['username'] = 'root';
+////    $config['password'] = 'admin';
+//} else if (YII_ENV == 'test') {
+//    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
+//    $config['username'] = 'root';
+//    $config['password'] = 'admin';
+//} else if (YII_ENV == 'prod') {
+//    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
+//    $config['username'] = 'root';
+//    $config['password'] = 'admin';
+//}
+
+// $config['dsn'] = 'mysql:host=203.109.113.157;dbname=bridecycle';
+// $config['username'] = 'bridecycle';
+// $config['password'] = 'bridecycle@#!2021';
+
+$config['dsn'] = 'mysql:host=127.0.0.1;dbname=bridecycle';
+$config['username'] = 'root';
+$config['password'] = 'admin';
+
 
 return $config;
