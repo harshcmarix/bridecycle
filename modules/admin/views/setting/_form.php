beb11994c81235839f3b71e9519ee93a267a3ed9
Harshil <harshil.cmarix@gmail.com>
2021-10-20 17:25:00 +0530

    Changes regarding API and Backend admin panel

--- modules/admin/views/setting/_form.php
+++ modules/admin/views/setting/_form.php
@@ -12,9 +12,9 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-<!--    --><?php //echo $form->field($model, 'transaction_fees')->textInput(['maxlength' => true]) ?>
+    <!--    --><?php //echo $form->field($model, 'transaction_fees')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'km_range')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'km_range')->textInput(['maxlength' => true])->label(strtoupper($model->getAttributeLabel('km_range')) . " (for find the tailor)") ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
