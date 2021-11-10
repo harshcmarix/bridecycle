5f6b7eb419ef6060f22ca7dc9cb2077cdc77c983
ctpl205 <chiragg.cmarix@gmail.com>
2021-11-10 10:53:27 +0530

    file upload and DB changes.

---
+++ config/db_bkp_10-11-2021.php
@@ -0,0 +1,41 @@
+<?php
+
+$config = [
+    'class' => 'yii\db\Connection',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 600,
+    'schemaCache' => 'cache',
+    'enableQueryCache' => true,
+    'queryCacheDuration' => 3600,
+];
+
+//if (YII_ENV == 'dev') {
+//    /** Stage server */
+//    $config['dsn'] = 'mysql:host=203.109.113.157;dbname=bridecycle';
+//    $config['username'] = 'bridecycle';
+//    $config['password'] = 'bridecycle@#!2021';
+//    /** local */
+////    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
+////    $config['username'] = 'root';
+////    $config['password'] = 'admin';
+//} else if (YII_ENV == 'test') {
+//    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
+//    $config['username'] = 'root';
+//    $config['password'] = 'admin';
+//} else if (YII_ENV == 'prod') {
+//    $config['dsn'] = 'mysql:host=localhost;dbname=bridecycle';
+//    $config['username'] = 'root';
+//    $config['password'] = 'admin';
+//}
+
+// $config['dsn'] = 'mysql:host=203.109.113.157;dbname=bridecycle';
+// $config['username'] = 'bridecycle';
+// $config['password'] = 'bridecycle@#!2021';
+
+$config['dsn'] = 'mysql:host=127.0.0.1;dbname=bridecycle';
+$config['username'] = 'root';
+$config['password'] = 'admin';
+
+
+return $config;
